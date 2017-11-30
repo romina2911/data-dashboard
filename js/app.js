73e7a6083fe7ed7b386d2a75880aa9a3b6006066
romina2911 <romina.hp@gmail.com>
2017-11-30 18:19:38 -0500

    DataDashboard - Romina Herrera Y Franshesca Quezada

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 
 window.addEventListener ('load', function(){
   var selectProm = document.getElementById('select-prom');
-  selectProm.addEventListener('click', function() {
+  selectProm.addEventListener('click', function(){
     var menu = document.createElement('div');
     var sedes = Object.keys(data);
     // ['ARQ', 'LIM', 'CDMX', 'SCL']
@@ -23,6 +23,7 @@ window.addEventListener ('load', function(){
       newUl.appendChild(nameSede);
 
       var bootcamp = Object.keys(data[sedes[i]]);
+
       for(var j = 0; j < bootcamp.length; j++) {
         var newLi = document.createElement('li');
         newLi.innerHTML= bootcamp[j];
@@ -31,6 +32,7 @@ window.addEventListener ('load', function(){
       }
       menu.appendChild(newUl);
     }
+
     selectProm.appendChild(menu);
   });
 
@@ -43,33 +45,8 @@ window.addEventListener ('load', function(){
       console.log(data[sede])}
       
     }); 
-    
-    /*var sedes = Object.keys(data);
-    var menu = document.createElement('div');
-    for(var i = 0; i < sedes.length; i++) {
-      var bootcamp = Object.keys(data[sedes[i]]);
-      for(var j = 0; j < bootcamp.length; j++) {
-        var newLi = document.createElement('li');
-        var studentsData = Object.values(data[sedes[i]]);
-        for(var k = 0; k < studentsData.length; k++) {
-          var newLiStudents = document.createElement('li');
-            newLiStudents.innerHTML= studentsData[k];
-            newLiStudents.id = studentsData[k];
-            newLi.appendChild(newLiStudents);
-          };
-
-      };
-    };
-
-    students.appendChild(menu);*/       
+        
           
       
 });
-<<<<<<< HEAD
 /*>>>>>>> fbcb085f7d27a89cd9d0c6cb45b6724b1e084a69*/
-=======
-/*<<<<<<< HEAD
-/*>>>>>>> fbcb085f7d27a89cd9d0c6cb45b6724b1e084a69
-=======
-/*>>>>>>> 4ee94b6cfc57e7e4d8a8a3eddc0806ea93021b35*/
->>>>>>> c5da3d30becb0dcb49ebf622b904887819dfdae2
